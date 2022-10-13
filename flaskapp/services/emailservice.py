04679ff4c29359b7c32d62f8c6d64f64b7a061c8
Esteban <esteban.villegas92@hotmail.com>
2022-10-12 23:36:22 -0500

    Made email server settings configurable

--- flaskapp/services/emailservice.py
+++ flaskapp/services/emailservice.py
@@ -7,16 +7,20 @@ class EmailService:
     def __init__(self) -> None:
         self.email_username = current_app.config["EMAIL_USERNAME"]
         self.email_password = current_app.config["EMAIL_PASSWORD"]
+        self.email_host = current_app.config["EMAIL_HOST"]
+        self.email_port = current_app.config["EMAIL_PORT"]
+        self.email_enabled = current_app.config["EMAIL_ENABLED"]
     
     def send_email(self, recipient_email, subject, message_body):
-       email = EmailMessage()
-       email["From"] = self.email_username
-       email["To"] = recipient_email
-       email["Subject"] = subject
-       email.set_content(message_body)
+        if self.email_enabled == "True":
+            email = EmailMessage()
+            email["From"] = self.email_username
+            email["To"] = recipient_email
+            email["Subject"] = subject
+            email.set_content(message_body)
 
-       smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
-       smtp.starttls()
-       smtp.login(self.email_username, self.email_password)
-       smtp.sendmail(self.email_username, recipient_email, email.as_string())
-       smtp.quit()
+            smtp = smtplib.SMTP(self.email_host, port=self.email_port)
+            smtp.starttls()
+            smtp.login(self.email_username, self.email_password)
+            smtp.sendmail(self.email_username, recipient_email, email.as_string())
+            smtp.quit()
